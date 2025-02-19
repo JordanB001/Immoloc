0414c357d0dedc9be1f56779a77ae847bee961ed
JordanB <jordan.bigre@croisix.com>
2025-02-19 14:43:44 +0100

    ff

--- immoloc/routes/estimate.py
+++ immoloc/routes/estimate.py
@@ -1,38 +1,51 @@
-from flask import Blueprint, request
+import re
+from flask import Blueprint, json, jsonify, request
 from ..model import model, client
 
 bp = Blueprint("estimate", __name__)
+
 
 @bp.route("/estimate", methods=["POST"])
 def estimate():
     try:
         data: dict = request.get_json()
         
-        if data["real_estate_ad"]:
+        if "real_estate_ad" in data:
             if isinstance(data["real_estate_ad"], str):
                 real_estate_ad: str = data["real_estate_ad"]
             else:
-                return "Data error: must be a string", 400
+                return jsonify({"error": "Data error: must be a string"}), 400
         else:
-            return "Data missing: real_estate_ad", 400
+            return jsonify({"error": "Data missing: real_estate_ad"}), 400
     except Exception as e:
-        return f"Data error or data missing: {e}", 400
-    
-    
+        return jsonify({"error": f"Data error or data missing: {e}"}), 400
+        
     try:
-        chat_reponse = client.chat.complete(
+        chat_response = client.chat.complete(
             model= model,
             messages = [
                 {
                     "role": "user",
-                    "content": f"Calculate a price range for a property. If in the ad there is no type of property (house/apartment), surface area in m² or location of the property (City + Postal Code) then you just say 'PAS ASSEZ DE CRITÈRES'. In French. Here is the announcement: {real_estate_ad}"
+                    "content": f"Calculate a price range for a property. In French. Here is the announcement: {real_estate_ad},  Only If there is no place/location then display 'Entrer la localisation', Only If there is no surface (m2 / m² / m 2) then display 'Entrer la surface du bien', Only If there is no type of property then display 'Entrer le type du bien'"
                 }
             ]
         )
     except Exception as e:
         return f"{e}", 400
     
+    if "Entrer la localisation" in chat_response.choices[0].message.content:
+        return jsonify({"error": "Data missing: location"}), 400
+    if "Entrer la surface du bien" in chat_response.choices[0].message.content:
+        return jsonify({"error": "Data missing: surface area"}), 400
+    if "Entrer le type du bien" in chat_response.choices[0].message.content:
+        return jsonify({"error": "Data missing: type of property"}), 400
+    
+    match = re.search(r'\`\`\`json\s*([\s\S]*?)\s*\`\`\`', chat_response.choices[0].message.content)
+    if match:
+        json_str = match.group(1)
+        data_result = json.loads(json_str)
+        print(data)
     
     
-    return chat_reponse.choices[0].message.content, 200
+    return data_result , 200
     
